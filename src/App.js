03b1745cdaf3be6bde7fdefe63d373a1f3b5306a
Isaac <isaacjqz@gmail.com>
2023-03-15 15:28:55 -0400

    Created Header, AvailableMeals, Meals, and Cart components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React, { Fragment } from "react";
+
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+
 function App() {
   return (
-    <div>
-      <h1>Food Ordering App</h1>
-    </div>
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+
+const MealItemForm = () => {
+  return (
+    <Fragment>
+      <form>
+        {/* <input>
+        <label>
+
+        </label>
+        </input> */}
+      </form>
+    </Fragment>
+  );
+};
+
+export default MealItemForm;
